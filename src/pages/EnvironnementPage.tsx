d5d0d40fd556da3e8decc8a0c9fce93ce271260f
urbatec40-imd <urbatec40@gmail.com>
2026-08-19 15:39:38 +0100

    Fix environnement module initial state

--- src/pages/EnvironnementPage.tsx
+++ src/pages/EnvironnementPage.tsx
@@ -1,1 +1,5 @@
-export { EnvironnementPageV5 as EnvironnementPage } from './EnvironnementPageV5';
+import { EnvironnementPageV5 } from './EnvironnementPageV5';
+
+export function EnvironnementPage(props: Omit<React.ComponentProps<typeof EnvironnementPageV5>, 'initialPrestation'>) {
+  return <EnvironnementPageV5 {...props} initialPrestation="" />;
+}
