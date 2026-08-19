626923fdd16eddcae9ffd9d11d88a375f363842a
urbatec40-imd <urbatec40@gmail.com>
2026-08-19 19:12:09 +0100

    feat(env): switch environment module to V10 matrix flow

--- src/pages/EnvironnementPage.tsx
+++ src/pages/EnvironnementPage.tsx
@@ -1,6 +1,6 @@
 import type { ComponentProps } from 'react';
-import { EnvironnementPageV9 } from './EnvironnementPageV9';
+import { EnvironnementPageV10 } from './EnvironnementPageV10';
 
-export function EnvironnementPage(props: ComponentProps<typeof EnvironnementPageV9>) {
-  return <EnvironnementPageV9 {...props} />;
+export function EnvironnementPage(props: ComponentProps<typeof EnvironnementPageV10>) {
+  return <EnvironnementPageV10 {...props} />;
 }
