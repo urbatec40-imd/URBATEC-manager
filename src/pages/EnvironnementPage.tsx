3053d829b16d7b5aebd0199112106454fd5770be
urbatec40-imd <urbatec40@gmail.com>
2026-08-19 15:39:44 +0100

    Fix environnement module initial state typing

--- src/pages/EnvironnementPage.tsx
+++ src/pages/EnvironnementPage.tsx
@@ -1,5 +1,6 @@
+import type { ComponentProps } from 'react';
 import { EnvironnementPageV5 } from './EnvironnementPageV5';
 
-export function EnvironnementPage(props: Omit<React.ComponentProps<typeof EnvironnementPageV5>, 'initialPrestation'>) {
+export function EnvironnementPage(props: Omit<ComponentProps<typeof EnvironnementPageV5>, 'initialPrestation'>) {
   return <EnvironnementPageV5 {...props} initialPrestation="" />;
 }
